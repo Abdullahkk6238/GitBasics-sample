7361b20f9c4967c743c2720c77d8b8bc4807e79b
Abdullah kk <abdullahkk6238@gmail.com>
2025-02-07 15:33:24 +0530

    Python initial commit

---
+++ Day1.py
@@ -0,0 +1,52 @@
+'''
+name = "Abdullah"
+age = 21
+price = 25.99
+old = False
+a = None
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(old))
+print(type(a))
+'''
+
+
+'''
+# Print Sum
+
+a = 1
+b = 2
+sum = a + b
+print(sum)
+'''
+
+
+'''
+# Python is an implicit typed language
+
+* prints times txt
+a,b = 2, 3
+txt= "@"
+print(a*txt*b)
+
+
+two strings can be added with + (concatenation)
+a,b = "2", 3
+txt = "@"
+print((a+txt)*b)
+'''
+
+
+'''
+// -> integer division
+** -> Power 
+
+int and float -> float
+
+divide two int -> float
+
+floor() (integer less or equal to )is same as integer division
+'''
+
